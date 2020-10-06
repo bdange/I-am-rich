975e06c46c5edc08a50c72105fcf4525631e06c6
benjamin dange <benjamin.dange@gmail.com>
2020-10-06 18:08:10 -0400

    First app created on Swift

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Benjamin Dange on 24/09/2020.
+//  Copyright © 2020 Benjamin Dange. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
